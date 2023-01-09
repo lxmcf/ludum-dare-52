55c61e494b4d817b1e2d3c108a7b7406d355bdc5
lxmcf <lxmcf20@gmail.com>
2023-01-09 22:23:02 +1100

    It do be done

--- src/main.c
+++ src/main.c
@@ -2,14 +2,13 @@
 // NOTE!
 //
 // Please forgive my awful code, the whole idea of this was to bash it out quickly
-// and not get bogged down in pretty code or massive optimisations.
+// and not get bogged down in pretty code or massive optimisations... Please...
 //
 // Mamaaa, Just killed a thread...
+//
+// Graphics:    https://vryell.itch.io/
+// Audio:       https://www.kenney.nl/
 //----------------------------------------------------------------------------------
-
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "raylib.h"
 #include "raymath.h"
 
@@ -28,11 +27,17 @@ const float VIRTUAL_RATIO = (float)VIRTUAL_WIDTH / (float)VIRTUAL_HEIGHT;
 #define TARGET_FPS 60
 #define PLAYER_SPEED 100
 
-#define GROUND_GREEN CLITERAL(Color){ 139, 147, 64, 255 }
+#define GROUND_GREEN (Color){ 139, 147, 64, 255 }
 #define MAX_TEXTURES 32
-#define MAX_SOUNDS 4
+#define MAX_SOUNDS 6
 #define MAX_COLLIDERS 12
 #define MAX_PLOTS 20
+
+#define GROWTH_RATE 0.35f
+#define GROWTH_MAX 3.0f
+
+#define POINTS_GOAL 150
+#define MAX_ROTTEN_CROPS 5
 
 #define FALSE 0
 #define TRUE 1
@@ -53,6 +58,12 @@ typedef enum {
     IDLE,
     MOVING
 } RH_PlayerState;
+
+typedef enum {
+    RUNNING,
+    WON,
+    LOST
+} RH_GameState;
 
 typedef enum {
     RIGHT,
@@ -80,14 +91,10 @@ typedef struct FarmPlot {
     RH_Crop currentCrop;
 
     float cropTimer;
-    float cropTimerMax;
-
-    float cropGrowthRate;
 
     int interactable;
-
-    int watered;
-    int purchased;
+    int cropGrown;
+    int cropRotten;
 } FarmPlot;
 
 //----------------------------------------------------------------------------------
@@ -122,14 +129,28 @@ static int cropCorn;
 static int cropTomato;
 static int cropTurnip;
 
+static int sellBox;
+
 static int barn;
 
 static int farmPlot;
 
 static int interact;
 
+static int error;
+
+static int interface;
+static int coin;
+static int crop;
+
 // Sound ID's
-static int pickupCoin;
+static int harvestGrown;
+static int harvestRotten;
+
+static int plantSeed;
+
+static int gameWon;
+static int gameLost;
 
 // Player Animations
 static int walkingAnimation[4];
@@ -141,13 +162,17 @@ static float currentFrame = 0.0f;
 static Vector2 position;
 static RH_PlayerState state;
 
-static int totalCoins;
+static RH_GameState gameState;
 
-static int beetrootSeeds;
-static int carrotSeeds;
-static int cornSeeds;
-static int tomatoSeeds;
-static int turnipSeeds;
+static int totalPoints;
+
+static int totalCrops;
+static int maxCrops;
+
+static int harvestedRottenCrops;
+
+static float secondsSurvived;
+static int gameStarted;
 
 // Asset Pools
 static TextureSheet texturePool[MAX_TEXTURES];
@@ -232,15 +257,14 @@ int CreateFarmPlot (Vector2 position) {
     FarmPlot plot = { 0 };
 
     plot.position = (Vector2) { position.x, position.y };
-    plot.purchased = FALSE;
-    plot.watered = FALSE;
     plot.interactable = FALSE;
 
-    plot.cropGrowthRate = 0.1f;
     plot.cropTimer = 0.0f;
-    plot.cropTimerMax = 3.0f;
 
     plot.currentCrop = NONE;
+
+    plot.cropGrown = FALSE;
+    plot.cropRotten = FALSE;
 
     farmPlots[farmPlotsCount] = plot;
 
@@ -255,12 +279,18 @@ static void InitGame (void);
 static void UpdateGame (void);
 static void DrawGame (void);
 static void UnloadGame (void);
+static void ResetGame (void);
+
+static void DrawEndGame (void);
 
 //----------------------------------------------------------------------------------
 // Main loop
 //----------------------------------------------------------------------------------
 int main (int argc, const char* argv[]) {
     InitWindow (WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+
+    SetTargetFPS (TARGET_FPS);
+
     InitAudioDevice ();
 
     LoadGame ();
@@ -279,10 +309,10 @@ int main (int argc, const char* argv[]) {
         EndTextureMode ();
 
         BeginDrawing ();
-            ClearBackground (RED);
+            ClearBackground (BLACK);
 
             BeginMode2D (screenSpaceCamera);
-                DrawTexturePro (targetTexture.texture, sourceRectangle, destinationRectangle, (Vector2){0.0f, 0.0f}, 0.0f, WHITE);
+                DrawTexturePro (targetTexture.texture, sourceRectangle, destinationRectangle, (Vector2){0.0f, 0.0f}, 0.0f, (Color){ 255, 255, 255, 255 });
             EndMode2D ();
         EndDrawing ();
     }
@@ -311,6 +341,10 @@ static void LoadGame (void) {
     cropTomato = LoadTexturesheet ("data/sprites/crops/tomato.png", 3, (Vector2){ 8.0f, 14.0f });
     cropTurnip = LoadTexturesheet ("data/sprites/crops/turnip.png", 3, (Vector2){ 8.0f, 14.0f });
 
+    sellBox = LoadTexturesheet ("data/sprites/box.png", 2, Vector2Zero ());
+
+    interface = LoadTexturesheet ("data/sprites/interface.png", 1, (Vector2){ 0.0f, 24.0f });
+
     farmPlotsCrops[BEETROOT] = cropBeetroot;
     farmPlotsCrops[CARROT] = cropCarrot;
     farmPlotsCrops[CORN] = cropCorn;
@@ -321,15 +355,21 @@ static void LoadGame (void) {
     farmPlot = LoadTexturesheet ("data/sprites/plot.png", 2, (Vector2){ 7.0f, 7.0f });
     interact = LoadTexturesheet ("data/sprites/interact.png", 1, (Vector2){ 6.0f, 12.0f });
 
-    pickupCoin = LoadSoundFile ("data/sounds/coin.ogg");
+    coin = LoadTexturesheet ("data/sprites/coin.png", 1, (Vector2){ 0.0f, 24.0f });
+    crop = LoadTexturesheet ("data/sprites/crop.png", 1, (Vector2){ 0.0f, 24.0f });
+
+    harvestGrown = LoadSoundFile ("data/sounds/harvest_grown.ogg");
+    harvestRotten = LoadSoundFile ("data/sounds/harvest_rotten.ogg");
+    plantSeed = LoadSoundFile ("data/sounds/plant_seed.ogg");
+    error = LoadSoundFile ("data/sounds/error.ogg");
+
+    gameWon = LoadSoundFile ("data/sounds/game_won.ogg");
+    gameLost = LoadSoundFile ("data/sounds/game_lost.ogg");
+
+    targetTexture = LoadRenderTexture (VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
 }
 
 static void InitGame (void) {
-    SetTargetFPS (TARGET_FPS);
-
-    // Target Textures
-    targetTexture = LoadRenderTexture (VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
-
     // Camera
     worldSpaceCamera.zoom = 1.0f;
     screenSpaceCamera.zoom = 1.0f;
@@ -353,101 +393,153 @@ static void InitGame (void) {
     position = (Vector2){ VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2 };
     state = IDLE;
 
-    beetrootSeeds = 0;
-    carrotSeeds = 0;
-    cornSeeds = 0;
-    tomatoSeeds = 0;
-    turnipSeeds = 0;
+    totalPoints = 0;
+    maxCrops = 5;
+
+    harvestedRottenCrops = 0;
+
+    gameStarted = FALSE;
+    gameState = RUNNING;
 
     // Collisions
     collisions[collisionsCount] = (Rectangle){ 0, 0, texturePool[barn].frameWidth + 4, texturePool[barn].frameHeight + 2 };
     collisionsCount++;
 
+    collisions[collisionsCount] = (Rectangle){ 0, texturePool[barn].frameHeight, texturePool[sellBox].frameWidth, texturePool[sellBox].frameHeight };
+    collisionsCount++;
+
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 10; j++) {
+            if (j % 3 == 0) continue;
             CreateFarmPlot ((Vector2){(14.0f * 16.0f) + (i * 16.0f), (j * 16.0f) + 24.0f });
         }
     }
 }
 
 static void UpdateGame (void) {
-    int xaxis = IsKeyDown (KEY_D) - IsKeyDown (KEY_A);
-    int yaxis = IsKeyDown (KEY_S) - IsKeyDown (KEY_W);
+    if (gameState == RUNNING) {
+        int xaxis = IsKeyDown (KEY_D) - IsKeyDown (KEY_A);
+        int yaxis = IsKeyDown (KEY_S) - IsKeyDown (KEY_W);
 
-    if (xaxis != 0 || yaxis != 0) {
-        Vector2 velocity = { (xaxis * PLAYER_SPEED) * GetFrameTime (), (yaxis * PLAYER_SPEED) * GetFrameTime () };
+        int canMove = true;
 
-        Vector2 newPosition = Vector2Add (position, velocity);
-
-        for (int i = 0; i < collisionsCount; i++) {
-            if (!CheckCollisionPointRec(newPosition, collisions[i])) {
-                position = newPosition;
+        if (xaxis != 0 || yaxis != 0) {
+            if (!gameStarted) {
+                gameStarted = TRUE;
             }
-        }
 
-        // Get needed animation
-        float direction = Vector2Angle (Vector2Zero (), velocity) * RAD2DEG;
-        direction = direction <= 0 ? fabsf (-direction) : 360.0f - direction;
+            Vector2 velocity = { (xaxis * PLAYER_SPEED) * GetFrameTime (), (yaxis * PLAYER_SPEED) * GetFrameTime () };
 
-        currentAnimation = (int)direction / 90;
+            Vector2 newPosition = Vector2Add (position, velocity);
 
-        state = MOVING;
-    } else {
-        state = IDLE;
-    }
-
-    for (int i = 0; i < farmPlotsCount; i++) {
-        // NOTE: Try to ignore this...
-        farmPlots[i].interactable = Vector2Distance (position, farmPlots[i].position) < 8.0f
-                                                    && (farmPlots[i].cropTimer >= farmPlots[i].cropTimerMax - 1.0f ||
-                                                        farmPlots[i].currentCrop == NONE);
-
-        if (farmPlots[i].interactable) {
-            if (IsKeyPressed(KEY_E)) {
-                if (farmPlots[i].cropTimer >= farmPlots[i].cropTimerMax - 1.0f) {
-                    farmPlots[i].currentCrop = NONE;
-                    farmPlots[i].cropTimer = 0.0f;
-
-                    PlaySoundFile (pickupCoin);
+            for (int i = 0; i < collisionsCount; i++) {
+                if (CheckCollisionPointRec (newPosition, collisions[i])) {
+                    canMove = false;
                 }
             }
 
-            if (IsKeyPressed(KEY_ONE)) {
-                farmPlots[i].currentCrop = BEETROOT;
+            if (canMove) {
+                position = newPosition;
             }
 
-            if (IsKeyPressed(KEY_TWO)) {
-                farmPlots[i].currentCrop = CARROT;
+            // Get needed animation
+            float direction = Vector2Angle (Vector2Zero (), velocity) * RAD2DEG;
+            direction = direction <= 0 ? fabsf (-direction) : 360.0f - direction;
+
+            currentAnimation = (int)direction / 90;
+
+            state = MOVING;
+        } else {
+            state = IDLE;
+        }
+
+        for (int i = 0; i < farmPlotsCount; i++) {
+            // NOTE: Try to ignore this...
+            farmPlots[i].interactable = Vector2Distance (position, farmPlots[i].position) < 8.0f && (farmPlots[i].cropGrown || farmPlots[i].currentCrop == NONE);
+
+            if (farmPlots[i].interactable) {
+                if (IsKeyPressed(KEY_E)) {
+                    if (farmPlots[i].cropTimer >= GROWTH_MAX - 1.0f) {
+                        if (farmPlots[i].cropRotten) {
+                            farmPlots[i].currentCrop = NONE;
+                            farmPlots[i].cropTimer = 0.0f;
+
+                            PlaySoundFile (harvestRotten);
+
+                            harvestedRottenCrops++;
+
+                            if (harvestedRottenCrops >= MAX_ROTTEN_CROPS) {
+                                gameState = LOST;
+
+                                PlaySoundFile (gameLost);
+                            }
+
+                            farmPlots[i].currentCrop = GetRandomValue (0, 4);
+                        } else {
+                            if (totalCrops < maxCrops) {
+                                farmPlots[i].currentCrop = NONE;
+                                farmPlots[i].cropTimer = 0.0f;
+
+                                PlaySoundFile (harvestGrown);
+
+                                farmPlots[i].currentCrop = GetRandomValue (0, 4);
+
+                                totalCrops++;
+                            } else {
+                                PlaySoundFile (error);
+                            }
+                        }
+
+                    } else {
+                        farmPlots[i].currentCrop = GetRandomValue (0, 4);
+
+                        PlaySoundFile (plantSeed);
+                    }
+                }
             }
 
-            if (IsKeyPressed(KEY_THREE)) {
-                farmPlots[i].currentCrop = CORN;
+            if (farmPlots[i].currentCrop != NONE) {
+                if (farmPlots[i].cropTimer < GROWTH_MAX) {
+                    farmPlots[i].cropTimer += GetFrameTime () * GROWTH_RATE;
+                }
             }
 
-            if (IsKeyPressed(KEY_FOUR)) {
-                farmPlots[i].currentCrop = TOMATO;
-            }
+            farmPlots[i].cropGrown = farmPlots[i].cropTimer >= GROWTH_MAX - 1.0f;
+            farmPlots[i].cropRotten = farmPlots[i].cropTimer >= GROWTH_MAX;
+        }
 
-            if (IsKeyPressed(KEY_FIVE)) {
-                farmPlots[i].currentCrop = TURNIP;
+        if (Vector2Distance (position, (Vector2){ 8, texturePool[barn].frameHeight}) < 24.0f && totalCrops > 0) {
+            if (IsKeyPressed (KEY_E)) {
+                totalPoints += totalCrops + (1 * totalCrops);
+
+                if (totalPoints >= POINTS_GOAL) {
+                    gameState = WON;
+
+                    PlaySoundFile (gameWon);
+                }
+
+                totalCrops = 0;
             }
         }
 
-        if (farmPlots[i].currentCrop != NONE) {
-            if (farmPlots[i].cropTimer < farmPlots[i].cropTimerMax) {
-                farmPlots[i].cropTimer += GetFrameTime () * farmPlots[i].cropGrowthRate;
-            }
+        currentFrame += (GetFrameTime () * 10.0f);
+
+        if (gameStarted) {
+            secondsSurvived += GetFrameTime ();
+        }
+    } else {
+        if (IsKeyPressed (KEY_SPACE)) {
+            ResetGame ();
         }
     }
-
-    currentFrame += (GetFrameTime () * 10.0f);
 }
 
 static void DrawGame (void) {
     DrawTexturesheet (barn, Vector2Zero (), barnColour);
+    DrawTexturesheet (sellBox, (Vector2){ 0.0f, texturePool[barn].frameHeight }, 0);
 
     for (int i = 0; i < farmPlotsCount; i++) {
-        DrawTexturesheet (farmPlot, farmPlots[i].position, farmPlots[i].watered);
+        DrawTexturesheet (farmPlot, farmPlots[i].position, 0);
 
         if (farmPlots[i].currentCrop != NONE) {
             if (farmPlots[i].cropTimer < 1.0f) {
@@ -462,10 +554,25 @@ static void DrawGame (void) {
         }
     }
 
+    if (Vector2Distance (position, (Vector2){ 8, texturePool[barn].frameHeight}) < 24.0f && totalCrops > 0) {
+        DrawTexturesheet (interact, Vector2Subtract (position, (Vector2){ 0.0f, 18.0f }), 0);
+    }
+
     if (state == IDLE) {
         DrawTexturesheet (idleAnimation[currentAnimation], position, (int)currentFrame);
     } else {
         DrawTexturesheet (walkingAnimation[currentAnimation], position, (int)currentFrame);
+    }
+
+    DrawTexturesheet (interface, (Vector2){ 0.0f, VIRTUAL_HEIGHT}, 0);
+    DrawTexturesheet (coin, (Vector2){ 0.0f, VIRTUAL_HEIGHT}, 0);
+    DrawTexturesheet (crop, (Vector2){ 64.0f, VIRTUAL_HEIGHT}, 0);
+
+    DrawText (TextFormat("%d/%d", totalPoints, POINTS_GOAL), 20, VIRTUAL_HEIGHT - 16, 8, WHITE);
+    DrawText (TextFormat("%d/%d", totalCrops, maxCrops), 84, VIRTUAL_HEIGHT - 16, 8, WHITE);
+
+    if (gameState != RUNNING) {
+        DrawEndGame ();
     }
 }
 
@@ -475,4 +582,54 @@ static void UnloadGame (void) {
     for (int i = 0; i < textureCount; i++) {
         UnloadTexturesheet (texturePool[i]);
     }
+
+    for (int i = 0; i < soundCount; i++) {
+        UnloadSound (soundPool[i]);
+    }
+}
+
+static void ResetGame (void) {
+    barnColour = GetRandomValue (0, 5);
+
+    gameState = RUNNING;
+
+    // Player Values
+    position = (Vector2){ VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2 };
+    state = IDLE;
+
+    totalPoints = 0;
+    maxCrops = 5;
+
+    secondsSurvived = 0.0f;
+    harvestedRottenCrops = 0;
+
+    totalCrops = 0;
+
+    gameStarted = FALSE;
+
+    for (int i = 0; i < farmPlotsCount; i++) {
+        farmPlots[i].interactable = FALSE;
+
+        farmPlots[i].cropTimer = 0.0f;
+
+        farmPlots[i].currentCrop = NONE;
+
+        farmPlots[i].cropGrown = FALSE;
+        farmPlots[i].cropRotten = FALSE;
+    }
+}
+
+static void DrawEndGame (void) {
+    DrawRectangle (0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT, (Color){ 0, 0, 0, 200 });
+
+    if (gameState == WON) {
+        DrawText ("CROPS SAVED!", 8, 8, 20, WHITE);
+    } else if (gameState == LOST) {
+        DrawText ("CROPS LOST!", 8, 8, 20, WHITE);
+    }
+
+    DrawText (TextFormat ("Total Points:\t%d", totalPoints - (totalPoints % POINTS_GOAL)), 8, 40, 10, WHITE);
+    DrawText (TextFormat ("Total Time:\t%d seconds", (int)secondsSurvived), 8, 56, 10, WHITE);
+
+    DrawText ("Press <SPACE> to restart!", 8, 168, 10, WHITE);
 }
